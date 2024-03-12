a67f37378e1d8bbc9f3919fdd848dbda9a3f3c7b
Batuhan KARACA <b.karaca.1999@gmail.com>
2024-03-12 17:58:29 +0300

    Character Controller

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [Header("Player Movement")]
+    public float playerSpeed = 1.9f;
+
+    [Header("Player Animator and Gravity")]
+    public CharacterController cC;
+
+    private void FixedUpdate()
+    {
+        playerMove();
+    }
+
+    void playerMove()
+    {
+        float horizontal_axis = Input.GetAxisRaw("Horizontal");
+        float vertical_axis = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal_axis, 0f, vertical_axis).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            cC.Move(direction * playerSpeed * Time.deltaTime);
+        }
+    }
+}
